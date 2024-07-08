a3447e8cbc21fb506ae6d33ccc959f86ca3f295b
sathyabalanb1 <sathyabalanb1@gmail.com>
2024-07-08 08:44:33 +0530

    First and Final commit:Implemented CORS and CSRF

---
+++ src/main/java/com/eazybytes/controller/LoansController.java
@@ -0,0 +1,28 @@
+package com.eazybytes.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.model.Loans;
+import com.eazybytes.repository.LoanRepository;
+
+@RestController
+public class LoansController {
+
+	@Autowired
+    private LoanRepository loanRepository;
+
+    @GetMapping("/myLoans")
+    public List<Loans> getLoanDetails(@RequestParam int id) {
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null ) {
+            return loans;
+        }else {
+            return null;
+        }
+    }
+}
